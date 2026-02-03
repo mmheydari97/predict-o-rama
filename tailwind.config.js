f216dd43b0d9b6db06f7f1d3ba54ad848d2585d3
Mohammad Heydari <heidarymm@yahoo.com>
2026-02-03 15:49:24 -0500

    dracula theme
    add dracula theme colors from plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const dracula = require('tailwind-dracula/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,23 +8,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        card: "hsl(var(--card))",
-        "card-foreground": "hsl(var(--card-foreground))",
-        primary: "hsl(var(--primary))",
-        "primary-foreground": "hsl(var(--primary-foreground))",
-        secondary: "hsl(var(--secondary))",
-        "secondary-foreground": "hsl(var(--secondary-foreground))",
-        muted: "hsl(var(--muted))",
-        "muted-foreground": "hsl(var(--muted-foreground))",
-        accent: "hsl(var(--accent))",
-        "accent-foreground": "hsl(var(--accent-foreground))",
-        destructive: "hsl(var(--destructive))",
-        "destructive-foreground": "hsl(var(--destructive-foreground))",
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
+        background: dracula.light,
+        foreground: dracula.darker,
+        card: dracula.light,
+        "card-foreground": dracula.darker,
+        primary: dracula.purple,
+        "primary-foreground": dracula.darker,
+        secondary: dracula.pink,
+        "secondary-foreground": dracula.darker,
+        muted: dracula.light,
+        "muted-foreground": dracula.darker,
+        accent: dracula.cyan,
+        "accent-foreground": dracula.darker,
+        destructive: dracula.red,
+        "destructive-foreground": dracula.light,
+        border: dracula.light,
+        input: dracula.light,
+        ring: dracula.purple,
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -31,5 +33,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-dracula')('dracula'),
+  ],
 }
